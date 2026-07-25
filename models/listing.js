25deda435a0f60d73a5fa413e7904bb88d11cdfb
Virat Pandey <viratpandey08@gmail.com>
2026-07-25 16:42:46 +0530

    Implement review system and validation

--- models/listing.js
+++ models/listing.js
@@ -1,29 +1,49 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./review.js");
 
-const listingSchema = new Schema ({
+const listingSchema = new Schema({
     title: {
         type: String,
         required: true,
     },
+
     description: String,
+
     image: {
-    filename: {
-        type: String,
-        default: "listingimage",
+        filename: {
+            type: String,
+            default: "listingimage",
+        },
+        url: {
+            type: String,
+            default:
+                "https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&w=800&q=60",
+        },
     },
-    url: {
-        type: String,
-        default:
-            "https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&w=800&q=60",
-    },
-},
+
     price: {
-  type: Number,
-},
+        type: Number,
+        required: true,
+        min: 0,
+    },
+
     location: String,
     country: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Review"
+        }
+    ]
+});
+
+listingSchema.post("findOneAndDelete", async (listing) => {
+    if(listing) {
+        await Review.deleteMany({_id: {$in: listing.reviews}});
+    }
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
+
 module.exports = Listing;
