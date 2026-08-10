2ed515bda385e1ad21052b9cb6fe7b3561ecf752
Virat Pandey <viratpandey08@gmail.com>
2026-08-11 01:26:11 +0530

    Implement sessions, cookies, and flash messages

--- routes/listing.js
+++ routes/listing.js
@@ -9,10 +9,10 @@ const Listing = require("../models/listing.js");
 const validateListing = (req,res,next) => {
     let {error} = listingSchema.validate(req.body);
     if(error) {
-        let errMsg = error.details.map((el) => el.message).join(",");
-        throw new ExpressError(400 , errMsg);
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400 , errMsg);
     } else {
-        next();
+    next();
     }
 };
 
@@ -31,6 +31,10 @@ router.get("/new", (req,res) => {
 router.get("/:id" , wrapAsync(async(req,res) => {
     let {id} = req.params;
     const listing = await Listing.findById(id).populate("reviews");
+    if(!listing) {
+        req.flash("error", "Listing you requested for does not exist!");
+        return res.redirect("/listings");
+    }
     res.render("listings/show.ejs" , {listing});
 }));
 
@@ -39,6 +43,7 @@ router.post("/" , validateListing ,
     wrapAsync(async(req,res,next) => {
     const newListing = new Listing(req.body.listing);
     await newListing.save();
+    req.flash("success", "New Listing Created!");
     res.redirect("/listings");
     })
 );
@@ -47,6 +52,10 @@ router.post("/" , validateListing ,
 router.get("/:id/edit", wrapAsync(async(req,res) => {
     let {id} = req.params;
     const listing = await Listing.findById(id);
+    if(!listing) {
+        req.flash("error", "Listing you requested for does not exist!");
+        return res.redirect("/listings");
+    }
     res.render("listings/edit.ejs" , { listing });
 }));
 
@@ -56,6 +65,7 @@ router.get("/:id/edit", wrapAsync(async(req,res) => {
 router.put("/:id", validateListing, wrapAsync(async (req, res) => {
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    req.flash("success", "Listing Updated!");
     res.redirect(`/listings/${id}`);
 }));
 
@@ -64,6 +74,7 @@ router.delete("/:id" , wrapAsync(async(req,res) => {
     let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
+    req.flash("success", "Listing Deleted!");
     res.redirect("/listings");
 }));
 
