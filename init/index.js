f73080fe20e321e4729745187c366711ba21e0ee
Virat Pandey <viratpandey08@gmail.com>
2026-09-07 10:43:07 +0530

    IMPLEMENTATION OF AUTHENTICATION AND AUTHORIZATION TO VERIFY IF THE USER IS LOGGED IN AND IF THE USER IS THE AUTHOR OF THE REVIEW BEFORE ALLOWING THEM TO DELETE IT.

--- init/index.js
+++ init/index.js
@@ -18,8 +18,9 @@ async function main() {
 
 const initDB  = async () => {
     await Listing.deleteMany({});
+    initData.data = initData.data.map((obj) => ({...obj , owner:"6a99bcac5195941d7f58a0dd"}));
     await Listing.insertMany(initData.data);
     console.log("data was intialized");
-}
+};
 
 initDB();
